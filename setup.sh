e26686609d0c247204c4a2a5e767dd059a2b18a5
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2026-01-15 11:39:08 +0000

    Rename repo & fix dep issues

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-rosdep update && cd /workspaces/RPW_ros2_ws/ && rosdep install --from-paths src --ignore-src -r -y && cd -
+rosdep update && cd /workspaces/RPW_vislam/cam_ws && rosdep install --from-paths src --ignore-src -r -y
+colcon build --symlink-install --continue-on-error
+
+cd /workspaces/RPW_vislam/slam_ws && rosdep install --from-paths src --ignore-src -r -y
 colcon build --symlink-install --continue-on-error
