e2d56a39d65f06631ab5733482d4d7aa76b27157
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2026-01-30 16:56:37 +0200

    Refactor launch file

--- cam_ws/src/imx219-83_stereo_camera_ros2/launch/stereo_camera.launch.py
+++ cam_ws/src/imx219-83_stereo_camera_ros2/launch/stereo_camera.launch.py
@@ -14,69 +14,73 @@ def generate_launch_description():
         'stereo_camera.yaml'
     ])
 
+    # =======================
+    # GSCAM nodes
+    # =======================
+    left_camera_publisher = Node(
+        package='gscam',
+        executable='gscam_node',
+        name='imx_219_left',
+        parameters=[camera_params],
+        remappings=[
+            ('/camera/image_raw', '/stereo/left/image_raw'),
+            ('/camera/camera_info', '/stereo/left/camera_info'),
+            ('/camera/image_raw/compressed',
+             '/stereo/left/image_raw/compressed'),
+            ('/camera/image_raw/compressedDepth',
+             '/stereo/left/image_raw/compressedDepth'),
+        ],
+        output='screen'
+    )
+
+    right_camera_publisher = Node(
+        package='gscam',
+        executable='gscam_node',
+        name='imx_219_right',
+        parameters=[camera_params],
+        remappings=[
+            ('/camera/image_raw', '/stereo/right/image_raw'),
+            ('/camera/camera_info', '/stereo/right/camera_info'),
+            ('/camera/image_raw/compressed',
+             '/stereo/right/image_raw/compressed'),
+            ('/camera/image_raw/compressedDepth',
+             '/stereo/right/image_raw/compressedDepth'),
+        ],
+        output='screen'
+    )
+
+    # =======================
+    # Image rectification
+    # =======================
+    rectification = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('stereo_image_proc'),
+                'launch',
+                'stereo_image_proc.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'left_namespace': '/stereo/left',
+            'right_namespace': '/stereo/right',
+            'launch_image_proc': 'true',
+            'approximate_sync': 'true'
+        }.items()
+    )
+
+    # =======================
+    # IMU wrapper node
+    # =======================
+    imu_publisher = Node(
+        package='imx219-83_stereo_camera_ros2',
+        executable='icm20948_node',
+        name='imu_publisher_node',
+        output='screen'
+    )
+
     return LaunchDescription([
-
-        # =======================
-        # GSCAM nodes
-        # =======================
-        Node(
-            package='gscam',
-            executable='gscam_node',
-            name='imx_219_left',
-            parameters=[camera_params],
-            remappings=[
-                ('/camera/image_raw', '/stereo/left/image_raw'),
-                ('/camera/camera_info', '/stereo/left/camera_info'),
-                ('/camera/image_raw/compressed',
-                 '/stereo/left/image_raw/compressed'),
-                ('/camera/image_raw/compressedDepth',
-                 '/stereo/left/image_raw/compressedDepth'),
-            ],
-            output='screen'
-        ),
-
-        Node(
-            package='gscam',
-            executable='gscam_node',
-            name='imx_219_right',
-            parameters=[camera_params],
-            remappings=[
-                ('/camera/image_raw', '/stereo/right/image_raw'),
-                ('/camera/camera_info', '/stereo/right/camera_info'),
-                ('/camera/image_raw/compressed',
-                 '/stereo/right/image_raw/compressed'),
-                ('/camera/image_raw/compressedDepth',
-                 '/stereo/right/image_raw/compressedDepth'),
-            ],
-            output='screen'
-        ),
-
-        # =======================
-        # Image rectification
-        # =======================
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('stereo_image_proc'),
-                    'launch',
-                    'stereo_image_proc.launch.py'
-                ])
-            ]),
-            launch_arguments={
-                'left_namespace': '/stereo/left',
-                'right_namespace': '/stereo/right',
-                'launch_image_proc': 'true',
-                'approximate_sync': 'true'
-            }.items(),
-        ),
-
-        # =======================
-        # IMU wrapper node
-        # =======================
-        Node(
-            package='imx219-83_stereo_camera_ros2',
-            executable='icm20948_node',
-            name='imu_publisher_node',
-            output='screen'
-        ),
+        left_camera_publisher,
+        right_camera_publisher,
+        rectification,
+        imu_publisher
     ])
