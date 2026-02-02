76818020d05fd4d20233c6683a9a33f52e971af9
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2026-02-02 11:42:13 +0000

    Update odomVINS param name

--- slam_ws/src/imx219_slam/launch/rtabmap_stereo_imu.launch.py
+++ slam_ws/src/imx219_slam/launch/rtabmap_stereo_imu.launch.py
@@ -50,7 +50,7 @@ def generate_launch_description():
         'approx_sync_max_interval': 0.03,
         'subscribe_rgbd': False,
         'Odom/Strategy': '9',
-        'OdomVINS/ConfigPath': '/workspaces/RPW_vislam/slam_ws/src/imx219_slam/config/vins_config.yaml'
+        'OdomVINSFusion/ConfigPath': '/workspaces/RPW_vislam/slam_ws/src/imx219_slam/config/vins_config.yaml'
     }
 
     slam_parameters = {
