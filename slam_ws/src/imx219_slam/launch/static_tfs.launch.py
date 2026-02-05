2ed4c1900338a854ae5fbe3d0ed800961ed5dab6
juusojarvenpaa <juuso.jarvenpaa@gmail.com>
2026-02-05 15:03:41 +0000

    Fix frame transforms

--- slam_ws/src/imx219_slam/launch/static_tfs.launch.py
+++ slam_ws/src/imx219_slam/launch/static_tfs.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     return LaunchDescription([
 
         # -------------------------------------------------
-        # base_link -> camera_link
+        # base_link -> imu_link
         # -------------------------------------------------
         Node(
             package='tf2_ros',
@@ -22,30 +22,30 @@ def generate_launch_description():
         ),
 
         # -------------------------------------------------
-        # camera_link -> left_camera_optical
+        # imu_link -> left_camera_optical
         # -------------------------------------------------
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='camera_to_left_optical',
             arguments=[
-                '-0.0345', '0.0', '0.0',
-                '0.0', '0.0', '0.0', '1.0',
+                '-0.0299', '0.0', '0.0',
+                '-0.0046', '0.0057', '-0.0045', '1.0',
                 'imu_link',
                 'imx_219_left_link'
             ]
         ),
 
         # -------------------------------------------------
-        # camera_link -> right_camera_optical
+        # imu_link -> right_camera_optical
         # -------------------------------------------------
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='camera_to_right_optical',
             arguments=[
-                '0.0345', '0.0', '0.0',  # baseline (meters!)
-                '0.0', '0.0', '0.0', '1.0',
+                '0.0299', '0.0', '0.0',  # baseline (meters!)
+                '-0.389', '0.009', '-0.004', '0.921',
                 'imu_link',
                 'imx_219_right_link'
             ]
